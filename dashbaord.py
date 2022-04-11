946c63169ccc76c1f6d718ea00f21385f6756278
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-11 23:53:46 +0000

    Update dashbaord.py

--- dashbaord.py
+++ dashbaord.py
@@ -18,6 +18,46 @@ app = Dash(__name__)
 app.title = 'Stock Alert Dashboard(v1.0)'
 
 def get_filtered_data():
-    df = pd.read_csv('data/stock.csv', header = None)
-    df.rename(columns = {0:'Stock Name',
-                }) 
+    df = pd.read_csv('data/stock.csv', header=None)
+    df.rename(columns={0: 'Stock Name',
+                       1: 'Previous Close',
+                       2: 'Current Price',
+                       3: 'Minimum(Day)',
+                       4: 'Maximum(Day)',
+                       5: 'Minimum(Year)',
+                       6: 'Maximum(Year)',
+                       7: 'Minimum(Threshold)',
+                       8: 'Maximum(Threshold)',
+                       9: 'Last Update',
+                       10: 'difference',
+                       11: 'difference(%)',
+                       12: 'buy',
+                       13: 'market',
+                       14: 'currency'}, inplace=True)
+
+
+    columns = ['Stock Name', 'Previous Close', 'Current Price', 'difference', 'difference(%)',
+               'Minimum(Day)', 'Maximum(Day)', 'Minimum(Year)', 'Maximum(Year)', 'Minimum(Threshold)', 'Maximum(Threshold)']
+    df = df.round(2)
+    df['Last Update'] = pd.to_datetime(df['Last Update'])
+    lastest_date = df.groupby(['Stock Name'])[
+        'Last Update'].max().reset_index()['Last Update']
+
+    filtered_df = df[df['Last Update'].isin(lastest_date)]
+    filtered_df.sort_values(by=['difference(%)'], inplace=True, ascending=False)
+    filtered_df['difference(%)'] = filtered_df['difference(%)'].apply(lambda x: '{} %'.format(x))
+    filtered_df["Last Update"] = filtered_df["Last Update"].apply(lambda x: x.strftime('%H:%M:%S'))
+    df["Last Update"] = df["Last Update"].apply(lambda x: x.strftime('%H:%M:%S'))
+
+    indian_stocks = filtered_df[filtered_df['market'] == 'IN']
+
+    buy_table = indian_stocks[indian_stocks['buy'] == True]
+    watch_table = indian_stocks[indian_stocks['buy'] == False]
+
+    buy_table = buy_table[columns]
+    watch_table = watch_table[columns]
+
+    us_stocks = filtered_df[filtered_df['market'] == 'US']
+    us_stocks = us_stocks[columns]
+
+    return df, buy_table, watch_table, us_stocks
