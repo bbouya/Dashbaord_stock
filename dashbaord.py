535044ee32026e64b332184655769263222a8f6f
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-11 23:51:17 +0000

    Update dashbaord.py

--- dashbaord.py
+++ dashbaord.py
@@ -1,4 +1,6 @@
 from logging import warning
+from pickle import NONE
+from tkinter import N
 import pytz
 import datetime
 import pandas as pd
@@ -9,3 +11,13 @@ import warnings
 warnings.filterwarnings('ignore')
 
 IST = pytz.timezone('ignore')
+
+today_date = datetime.datetime.now(IST).strftime('%d-%m-%Y')
+
+app = Dash(__name__)
+app.title = 'Stock Alert Dashboard(v1.0)'
+
+def get_filtered_data():
+    df = pd.read_csv('data/stock.csv', header = None)
+    df.rename(columns = {0:'Stock Name',
+                }) 
